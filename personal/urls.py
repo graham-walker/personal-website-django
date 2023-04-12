e8a134b1412bdb2ecff07e69004a819ed05b484f
Graham Walker <contact@gwalkerux.com>
2023-04-11 23:22:26 -0400

    Updated to Django 4.2

--- personal/urls.py
+++ personal/urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^posts/$', views.posts, name='posts'),
-    url(r'^posts/(?P<pk>\d+)/?$', views.post, name='post'),
-    url(r'^media/$', views.MediaView.as_view(), name='media'),
-    url(r'^delete/media/(?P<pk>\d+)/?$', views.delete, name='deleteMedia'),
-    url(r'^logout/?$', views.logoutView, name='logout'),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^posts/$', views.posts, name='posts'),
+    re_path(r'^posts/(?P<pk>\d+)/?$', views.post, name='post'),
+    re_path(r'^media/$', views.MediaView.as_view(), name='media'),
+    re_path(r'^delete/media/(?P<pk>\d+)/?$', views.delete, name='deleteMedia'),
+    re_path(r'^logout/?$', views.logoutView, name='logout'),
 ]
 
 if settings.DEBUG is True:
